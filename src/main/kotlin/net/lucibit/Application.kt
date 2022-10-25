57fac56d426b94d2494703b6a91f5b4e3e19cb78
Lucian Onea <lucionea@amazon.com>
2022-10-25 15:12:20 +0200

    upgrade to latest Ktor and Kotlin

---
+++ src/main/kotlin/net/lucibit/Application.kt
@@ -0,0 +1,14 @@
+package net.lucibit
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import net.lucibit.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureSerialization()
+        configureMonitoring()
+        configureTemplating()
+        configureRouting()
+    }.start(wait = true)
+}
---
+++ src/test/kotlin/net/lucibit/ApplicationTest.kt
@@ -0,0 +1,33 @@
+package net.lucibit
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.callloging.*
+import org.slf4j.event.*
+import io.ktor.server.request.*
+import io.ktor.server.plugins.callid.*
+import io.ktor.server.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import net.lucibit.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
